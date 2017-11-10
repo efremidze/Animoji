837ea5ae466c010b7e87ef158c00ddb44652b356
Lasha Efremidze <efremidzel@hotmail.com>
2017-11-09 19:41:41 -0800

    Testing

--- Sources/Animoji.swift
+++ Sources/Animoji.swift
@@ -9,22 +9,8 @@
 import Foundation
 import SceneKit
 
-//AvatarKit
-
 public class Animoji: SCNView {
     public class func foo() {
-        let object = NSClassFromString("AVTPuppet")
-        propertyNames(forClass: object!).forEach { print($0) }
+        
     }
-}
-
-func propertyNames(forClass: AnyClass) -> [String] {
-    var count = UInt32()
-    guard let properties = class_copyPropertyList(forClass, &count) else { fatalError("missing properties") }
-    let names: [String] = (0..<Int(count)).flatMap { i in
-        let property: objc_property_t = properties[i]
-        return String(cString: property_getName(property))
-    }
-    free(properties)
-    return names
 }
