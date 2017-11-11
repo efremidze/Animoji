216e78da098ec8db4402057a8ad84ea2dd0cfc54
Lasha Efremidze <efremidzel@hotmail.com>
2017-11-10 20:35:21 -0800

    Added headers

---
+++ Sources/Headers/AVTAvatarInstance.h
@@ -0,0 +1,12 @@
+//
+//  AVTAvatarInstance.h
+//  Animoji
+//
+//  Created by Lasha Efremidze on 11/10/17.
+//  Copyright © 2017 Lasha Efremidze. All rights reserved.
+//
+
+@import Foundation;
+
+@interface AVTAvatarInstance : NSObject
+@end
