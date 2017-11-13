2a9763fe1f7272cf741d33f2381f3c7c80dec49a
Lasha Efremidze <lasha.efremidze@ring.com>
2017-11-13 10:23:49 -0800

    Add collection view

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -21,4 +21,49 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.showsVerticalScrollIndicator = false
+            collectionView.register(Cell.self, forCellWithReuseIdentifier: "Cell")
+            layout?.itemSize = {
+                let itemsPerRow = 4
+                let availableWidth = self.view.bounds.width - CGFloat(itemsPerRow - 1) * layout!.minimumInteritemSpacing
+                let itemLength = floor(availableWidth / CGFloat(itemsPerRow))
+                return CGSize(width: itemLength, height: itemLength)
+            }()
+        }
+    }
+    
+    var layout: UICollectionViewFlowLayout? {
+        return collectionView.collectionViewLayout as? UICollectionViewFlowLayout
+    }
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Animoji.PuppetName.all.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+        let name = Animoji.PuppetName.all[indexPath.item]
+        cell.animoji.setPuppet(name: name)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let name = Animoji.PuppetName.all[indexPath.item]
+        animoji.setPuppet(name: name)
+    }
+}
+
+class Cell: UICollectionViewCell {
+    lazy var animoji: Animoji = { [unowned self] in
+        let animoji = Animoji(frame: self.contentView.bounds)
+        self.contentView.addSubview(animoji)
+        return animoji
+    }()
 }
