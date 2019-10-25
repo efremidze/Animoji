f2d99c781ce6e2268c4fb65399b2348a39538d39
Lasha Efremidze <efremidzel@hotmail.com>
2019-10-25 13:05:15 -0700

    separating swift and objc

--- Package.swift
+++ Package.swift
@@ -23,8 +23,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Animoji",
-            dependencies: [],
+            dependencies: ["AnimojiObjc"],
             path: "Sources"
+        ),
+        .target(
+            name: "AnimojiObjc",
+            dependencies: [],
+            path: "AnimojiObjc"
         )
     ]
 )
